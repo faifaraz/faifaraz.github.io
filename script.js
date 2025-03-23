c8741e7f807347f511b5d9ea70851efb4e1b1435
faifaraz <92971279+faifaraz@users.noreply.github.com>
2025-03-23 07:57:40 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,8 +2,6 @@ const works = {
     "task1": { "title": "Project 1", "url": "works/task1/index.html" },
     "task2": { "title": "Project 2", "url": "works/task2/index.html" },
     "task3": { "title": "Project 3", "url": "works/task3/index.html" },
-    "task4": { "title": "Project 4", "url": "works/task3/index.html" },
-
 };
 
 // Function to create the navigation menu
